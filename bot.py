30a8ff8da1c71d2abc63bf7ca8af9b95de853f20
li1553770945 <1553770945@qq.com>
2023-08-30 04:41:46 +0800

    feat(adapter):更改为mirai协议

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import nonebot
-from nonebot_adapter_walleq import Adapter
+from nonebot.adapters.mirai2 import Adapter as MIRAI2Adapter 
 # 初始化 NoneBot
 nonebot.init()
 
 # 注册适配器
 driver = nonebot.get_driver()
-driver.register_adapter(Adapter)
+driver.register_adapter(MIRAI2Adapter)
 
 # 在这里加载插件
 nonebot.load_builtin_plugins("echo")  # 内置插件
